e8dcd1760153a6b8746fa78ebf06f6947c7b4e30
felprangel <feliperangel897@gmail.com>
2025-05-19 21:46:49 -0300

    adiciona early return

--- src/main.c
+++ src/main.c
@@ -2,11 +2,12 @@
 
 #include "init.h"
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
         printf("USAGE: fit <command> [<args>]\n");
+        return 1;
     }
 
     initialize_repository();
