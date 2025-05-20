0cd8aeb96ab49220a19900d122c4ef014af94b63
felprangel <feliperangel897@gmail.com>
2025-05-19 21:37:58 -0300

    utiliza a função

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+#include "init.h"
+
 void main(int argc, char *argv[])
 {
     if (argc < 2)
     {
         printf("USAGE: fit <command> [<args>]\n");
     }
+
+    initialize_repository();
 
     printf("Hello World\n");
 }
