5966c104c29b34cde3ceeb76998cf049cc8f733b
felprangel <feliperangel897@gmail.com>
2025-05-21 20:19:45 -0300

    adiciona criação da pasta objects

--- src/init.c
+++ src/init.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
-#define FIT_DIRECTORY ".fit"
+#define FIT_DIR ".fit"
+#define FIT_OBJECTS_DIR ".fit/objects"
 #define DIRECTORY_PERMISSIONS 0755
 
 void initialize_repository(void)
 {
-    if (mkdir(FIT_DIRECTORY, DIRECTORY_PERMISSIONS) == 0)
+    if (mkdir(FIT_DIR, DIRECTORY_PERMISSIONS) == 0)
     {
-        printf("Pasta criada\n");
+        if (mkdir(FIT_OBJECTS_DIR, DIRECTORY_PERMISSIONS) == 0)
+        {
+            printf("Initialized empty Fit repository\n");
+        }
     } else
     {
-        printf("Erro ao criar pasta\n");
+        printf("Erro ao inicializar repositório fit\n");
     }
 }
